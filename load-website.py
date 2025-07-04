afc783c5abef1204d6cab85d5cbb907c4df59ba4
hfst.co <39383536+hfstco@users.noreply.github.com>
2025-07-04 20:01:14 +0200

    Set login timeout for pxssh connection in `load-website.py`

--- load-website.py
+++ load-website.py
@@ -70,7 +70,7 @@ def main():
     h2_perf_timings = []
 
     server = pxssh.pxssh(logfile=sys.stdout, encoding='utf-8')
-    server.login(server_config["hostname"], server_config["username"], server_config["port"])
+    server.login(server_config["hostname"], server_config["username"], server_config["port"], login_timeout=10)
 
     for i in range(10):
         for t in ["h2", "h3"]:
